3a5c7880b57b900a102c9963cba0197a540b405c
Nick Frances Hoeben <n.franceshoeben.student.tudelft.nl>
2023-05-11 14:51:56 +0200

    Working

--- main.py
+++ main.py
@@ -22,7 +22,19 @@ r0 = 0
 g0 = 0
 
 
-interior = upwardsIntegrator(r0,dr,M0,rho_0,R1,n)
+interior = upwardsIntegrator(r0,dr,M0,rho_0,n)
 
 interior2 = downwardsIntegrator(0,dr,rho_0,n,interior)
+
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,3,1)
+ax2 = fig.add_subplot(1,3,2)
+ax3 = fig.add_subplot(1,3,3)
+
+ax1.plot(interior2[:,1],interior2[:,0])
+ax2.plot(interior2[:,2],interior2[:,0])
+ax3.plot(interior2[:,3],interior2[:,0])
+
+plt.show()
 
--- tools.py
+++ tools.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 
-def upwardsIntegrator(r0, dr, M0, rho_0,R,n):
+def upwardsIntegrator(r0, dr, M0, rho_0,n):
 
     r = r0
     M = M0
@@ -17,9 +17,9 @@ def upwardsIntegrator(r0, dr, M0, rho_0,R,n):
         M += 4*np.pi*rho_0*(r**2)*dr
         g = G*M/(r**2)
 
-        interior[i,0] = R
+        interior[i,0] = r
         interior[i,1] = M
-        interior[i,2] = G
+        interior[i,2] = g
 
     return interior
 
@@ -30,8 +30,8 @@ def downwardsIntegrator(p0,dr,rho_0,n,interior):
 
     for i in range(n):
 
-        p += rho_0*interior[n-1,2]*dr
-        interiorNew[i,3] = p
+        p += rho_0*interior[n-i-1,2]*dr
+        interiorNew[n-i-1,3] = p
 
     return interiorNew
 
