8d41ded5f37e32d1cc5d36fe417febdcfaea5c9b
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2023-05-12 15:56:23 +0200

    Combine integration into layer function

--- main.py
+++ main.py
@@ -1,54 +1,73 @@
-import numpy as np
+# %%
 import matplotlib.pyplot as plt
-from tools import upwardsIntegrator, downwardsIntegrator
+import numpy as np
+import seaborn as sns
+from tools import integrate_layers
 
-n = 10000
+sns.set_theme(style="whitegrid", palette="Set2")
+pal = sns.color_palette("Set2")
 
+# %%
+# Titan values
+R = 2575e3
+g = 1.35
 
-#Input planet parameters
-R1 = 2575*1e3 #Change this
-R2 = 0#More layers
-R3 = 0#Even more
-G = 6.6743e-11
-
-
-rho_0 = 1880 #Density in layer 0
-rho_1 = 0 #Density in layer 1, etc
-
-#Initialise variables
-dr = R1/n
-M0 = 0
-r0 = 0
-g0 = 0
-
-
-interior = upwardsIntegrator(r0,dr,M0,rho_0,n)
-
-interior2 = downwardsIntegrator(0,dr,rho_0,n,interior)
-
-
-#Verification
-
-massError = interior2[-1,1] - 1345.70*1e20
-massErrorPercent = massError/(1345*1e20) * 100
-radiusError = interior2[-1,0] - R1
-radiusErrorPercent = (radiusError/R1) * 100
-
-print(f'Mass error: {massError} which is a percentage error of {massErrorPercent}%')
-print(f'Radius error: {radiusError} which is a percentage error of {radiusErrorPercent}%')
+# Layers from Grasset, 1998 (same as on Brightspace forum for reference)
+layers = [
+    # Iron core
+    [0, 910e3, 8000],
+    # Silicates
+    [910e3, 1710e3, 3300],
+    # HP ices + hydrates
+    [1710e3, 2200e3, 1310],
+    # Liquid layer
+    [2200e3, 2500e3, 1000],
+    # Ice 1 layer
+    [2500e3, 2575e3, 917],
+]
 
 
+values = integrate_layers(layers)
+rs = values[:, :, 0].flatten()
+ms = values[:, :, 1].flatten()
+gs = values[:, :, 2].flatten()
+ps = values[:, :, 3].flatten()
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1,3,1)
-ax2 = fig.add_subplot(1,3,2)
-ax3 = fig.add_subplot(1,3,3)
+total_m = ms[-1]
+p_center = ps[0]
+g_surface = gs[-1]
+print(f"Total mass: {total_m:.2e} kg")
+# TODO: for some reason the pressure is 1e3 times too large?
+print(f"Pressure at center: {p_center/1e6:.2e} MPa")
+print(f"Gravity at surface: {g_surface:.2f} m/s^2")
 
-ax1.plot(interior2[:,1],interior2[:,0])
-ax2.plot(interior2[:,2],interior2[:,0])
-ax3.plot(interior2[:,3],interior2[:,0])
+with plt.rc_context({"axes.grid": False}):
+    fig, axes = plt.subplots(ncols=3, figsize=(10, 6))
 
-plt.show()
+    axes[0].plot(ms, rs / 1e3, color=pal[0], lw=2)
+    axes[1].plot(gs, rs / 1e3, color=pal[1], lw=2)
+    axes[2].plot(ps / 1e6, rs / 1e3, color=pal[2], lw=2)
 
+    axes[0].set_ylabel("Radius [km]")
+    axes[0].set_xlabel("Mass [kg]")
+    axes[1].set_xlabel("Gravity [m/s^2]")
+    axes[2].set_xlabel("Pressure [MPa]")
 
+    num_layers = values.shape[0]
+    if num_layers > 1:
+        for i in range(num_layers - 1):
+            for ax in axes:
+                ax.axhline(
+                    layers[i + 1][0] / 1e3,
+                    color="k",
+                    alpha=0.5,
+                    dashes=(5, 5),
+                    ls="--",
+                    lw=1,
+                    zorder=1,
+                )
 
+    plt.tight_layout()
+    plt.show()
+
+# %%
--- tools.py
+++ tools.py
@@ -1,42 +1,87 @@
 import numpy as np
 
 
+def integrate_layers(layers, num_steps=1000):
+    """
+    Layers start from center:
+    [
+        [0, 500, 1000],
+        [500, 6371 * 1e3, 2000],
+    ]
 
-def upwardsIntegrator(r0, dr, M0, rho_0,n):
+    Returns:
+    [layer, step, [r, m, g, p]]
+    """
+    assert layers[0][0] == 0
+    values = np.zeros([len(layers), num_steps, 4])
 
+    for i, layer in enumerate(layers):
+        [r0, r1, rho_0] = layer
+        assert r1 > r0
+
+        if i > 0:
+            if not r0 == layers[i - 1][1]:
+                raise Exception("Layers must be contiguous")
+
+        if i < len(layers) - 1:
+            if not r1 == layers[i + 1][0]:
+                raise Exception("Layers must be contiguous")
+
+        dr = (r1 - r0) / num_steps
+        # if we are starting at the center, take 0, else take the last mass of the last layer
+        M = 0 if i == 0 else values[i - 1, -1, 1]
+        interior = integrate_upwards(r0, dr, M, rho_0, num_steps)
+
+        values[i, :, :] = interior
+
+    for j, layer in enumerate(reversed(layers)):
+        i = len(layers) - j - 1
+        [r0, r1, rho_0] = layer
+
+        assert r1 > r0
+        dr = (r1 - r0) / num_steps
+        # if we are starting at the surface, take 0, else take the last pressure of the last layer
+        p0 = 0 if j == 0 else values[i + 1, 0, 3]
+        ps = integrate_downwards(p0, dr, rho_0, values[i, :, :])
+
+        values[i, :, 3] = ps
+
+    return values
+
+
+def integrate_upwards(r0, dr, M0, rho_0, num_steps=1000):
+    """
+    Integrate a layer upwards and return [[r, M, g]]
+    """
     r = r0
     M = M0
-    G = G = 6.6743e-11
+    G = 6.6743e-11
 
-    interior = np.zeros([n,4])
+    interior = np.zeros([num_steps, 4])
 
-
-    for i in range(n):
-
+    for i in range(num_steps):
         r += dr
-        M += 4*np.pi*rho_0*(r**2)*dr
-        g = G*M/(r**2)
+        M += 4 * np.pi * rho_0 * (r**2) * dr
+        g = G * M / (r**2)
 
-        interior[i,0] = r
-        interior[i,1] = M
-        interior[i,2] = g
+        interior[i, 0] = r
+        interior[i, 1] = M
+        interior[i, 2] = g
 
     return interior
 
-def downwardsIntegrator(p0,dr,rho_0,n,interior):
 
-    p = p0
-    interiorNew = interior
+def integrate_downwards(p0, dr, rho_0, interior):
+    """
+    Integrate a layer downwards and return an array with the pressures
+    """
+    num_steps = len(interior)
+    ps = np.zeros(num_steps)
+    ps[0] = p = p0
 
-    for i in range(n):
+    for i in range(num_steps):
+        g = interior[num_steps - i - 1, 2]
+        ps[num_steps - i - 1] = p
+        p += rho_0 * g * dr
 
-        p += rho_0*interior[n-i-1,2]*dr
-        interiorNew[n-i-1,3] = p
-
-    return interiorNew
-
-
-
-
-
-
+    return ps
